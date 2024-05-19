5a92f5cbd9826c68dc21512c31b81661f2f61146
xtebaan <fabian.arcila@correounivalle.edu.co>
2024-05-19 11:03:49 -0500

    Corrigiendo  algunos errores

--- Reproductor.py
+++ Reproductor.py
@@ -1,13 +1,16 @@
+import os
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 from Tooltip import Tooltip
+from tkinter import ttk
 import pygame.mixer as mx
-import tkinter.ttk as ttk
 import pygame
 from tkinter import filedialog
 
 class Reproductor():
+
+
     def agregar_cancion(self):
         canciones = filedialog.askopenfilenames(filetypes=[("Archivos de audio", "*.mp3")])
         for cancion in canciones:
@@ -29,7 +32,7 @@ class Reproductor():
         self.duracion_cancion_actual = pygame.mixer.Sound(self.canciones[self.indice_cancion_actual]).get_length()
         self.deslizar.config(to=self.duracion_cancion_actual)
         self.actualizar_barra_progreso()
-        
+
         self.btnPause.config(state="normal")
         self.btnStop.config(state="normal")
         self.btnPlay.config(state="disabled")
@@ -42,7 +45,7 @@ class Reproductor():
             self.btnPause.config(state="disabled")
             self.btnPlay.config(state="normal")
             self.bandera = False
-    
+
     def stop(self, event):
         mx.music.stop()
         self.lblestado.config(text="Reproduccion Detenida...")
@@ -50,27 +53,27 @@ class Reproductor():
         self.btnStop.config(state="disabled")
         self.btnPlay.config(state="normal")
         self.deslizar.set(0)
-    
+
     def Adelantar(self, event):
         if mx.music.get_busy():
             nueva_posicion = self.obtener_posicion_actual() + 10
             self.reproducir_desde_posicion(nueva_posicion)
         else:
             print("No hay música reproduciéndose en este momento.")
-    
+
     def Retroceder(self, event):
         if mx.music.get_busy():
             nueva_posicion = self.obtener_posicion_actual() - 10
             self.reproducir_desde_posicion(nueva_posicion)
         else:
             print("No hay música reproduciéndose en este momento.")
-    
+
     def obtener_posicion_actual(self):
         if mx.music.get_busy():
             return mx.music.get_pos() / 1000
         else:
             return self.deslizar.get()
-    
+
     def reproducir_desde_posicion(self, nueva_posicion):
         if nueva_posicion < 0:
             nueva_posicion = 0
@@ -80,21 +83,21 @@ class Reproductor():
         mx.music.stop()
         mx.music.play(start=nueva_posicion)
         self.deslizar.set(nueva_posicion)
-    
+
     def Pasarcancion(self, event):
         mx.music.stop()
         self.indice_cancion_actual += 1
         if self.indice_cancion_actual >= len(self.canciones):
             self.indice_cancion_actual = 0
         self.play(event)
-    
+
     def DevolverCancion(self, event):
         mx.music.stop()
         self.indice_cancion_actual -= 1
         if self.indice_cancion_actual < 0:
             self.indice_cancion_actual = len(self.canciones) - 1
         self.play(event)
-    
+
     def actualizar_barra_progreso(self):
         if mx.music.get_busy():
             self.deslizar.set(self.obtener_posicion_actual())
@@ -102,13 +105,3 @@ class Reproductor():
 
 if __name__ == "__main__":
     Reproductor()
-
-        
-        
-
-
-           
-        
-
-
-
